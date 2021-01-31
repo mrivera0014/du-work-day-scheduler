66857b4b218f0b8ab051ae0026cbe1570f40a1f6
Marble <mrivera0014@gmail.com>
2021-01-30 18:16:04 -0700

    created all time blocks on html, added function to store user input in local storage and retireve info, added function to change colors of time blocks

---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+    $(".save").on("click", function () {
+        var userInput = $(this).siblings(".description").val()
+        var time = $(this).parent().attr("id")
+        localStorage.setItem(time, userInput)
+    })
+    function update() {
+        var current = moment().hours()
+        $(".time-block").each(function () {
+            var timeBlock = parseInt($(this).attr("id").split("-")[1])
+            if (timeBlock < current) {
+                $(this).addClass("past")
+            } else if (timeBlock === current) {
+                $(this).removeClass("past")
+                $(this).addClass("present")
+            } else {
+                $(this).removeClass("past")
+                $(this).removeClass("present")
+                $(this).addClass("future")
+            }
+        })
+    }
+
+    update()
+
+    var updateHour = setInterval(update, 15000)
+
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+    $("#currentDay").text(moment().format())
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"))
+    $("#currentDay").text(moment().format())
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"))
+    $("#currentDay").text(moment().format())
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"))
+    $("#currentDay").text(moment().format())
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"))
+    $("#currentDay").text(moment().format())
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"))
+    $("#currentDay").text(moment().format())
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"))
+    $("#currentDay").text(moment().format())
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"))
+    $("#currentDay").text(moment().format())
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"))
+    $("#currentDay").text(moment().format())
+})
